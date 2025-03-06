eb56c23fc78ee17c9d9ed5d680799d162b91f098
MCapredoni <144966520+MCapredoni@users.noreply.github.com>
2025-03-06 16:09:31 -0500

    Add files via upload

---
+++ convertLineSlope.js
@@ -0,0 +1,7 @@
+// Calculate the **slope** of a line. Variables are **rise** and **run**.
+
+function convertLineSlope (x1, y1, x2, y2){
+    return (y2 - y1) / (x2 - x1)
+}
+
+export {convertLineSlope}
